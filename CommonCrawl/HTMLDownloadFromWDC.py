b0b03c54285c2c1e4e67d5ce78d93f6bd95ac2c6
Michael <michaelhodgins@live.co.uk>
2025-07-04 17:11:14 +0100

    Added ways to save csv for domains visited
    Added many urls for each visit to a gz

--- CommonCrawl/HTMLDownloadFromWDC.py
+++ CommonCrawl/HTMLDownloadFromWDC.py
@@ -10,18 +10,46 @@ It downloads the raw HTML of the pages and saves them to a local directory.
 
 import random, re, gzip, pathlib, urllib.parse, requests
 from bs4 import BeautifulSoup
+import csv
+
+PRINT_URLS = False
+
+#Array of domains visited, imported from csv
+DOMAINS_VISITED = []
+with open("domains_visited.csv", "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        DOMAINS_VISITED.append(row[0])
 
 WDC_Subsets = [
+    "AdministrativeArea",
+    "Answer",
+    "CollegeOrUniversity",
+    "Country",
+    "EducationalOrganization",
     "LocalBusiness",
+    "Organization",
     "GovernmentOrganization",
-    "Movie"
+    "Person",
+    "Place"
+]
+NUM_SAMPLES = [ # how many web pages to take
+    0, # AdministrativeArea
+    0, # Answer
+    2, # CollegeOrUniversity
+    0, # Country
+    2, # EducationalOrganization
+    1, # LocalBusiness
+    2, # Organization
+    2, # GovernmentOrganization
+    0, # Person
+    2  # Place
 ]
 BASE_URL = ("https://data.dws.informatik.uni-mannheim.de/"
              "structureddata/2024-12/quads/classspecific")
 UA = 'cc-schemaxtract/1.0 (JSON-LD & microdata extractor; michaelhodgins@live.co.uk)'
-NUM_SAMPLES = 30 # how many web pages to take
 SEED = None
-MAX_SKIP = 100_000 # The max possible number of lines to skip in .gz files
+MAX_SKIP = 1_000_000 # The max possible number of lines to skip in .gz files
 OUT_DIR = pathlib.Path("./wdc_microdata_html")
 OUT_DIR.mkdir(exist_ok=True)
 for subset in WDC_Subsets:
@@ -38,33 +66,52 @@ def list_gz_files(subset: str) -> list[str]:
     soup = BeautifulSoup(res.text, "html.parser")
     return [a["href"] for a in soup.select("a[href$='.gz']")]
 
-def random_url_from_gz_file(subset: str, gz_file: str) -> str | None:
+def random_url_from_gz_file(subset: str, gz_file: str, num_urls: int) -> str | None:
     """Open the gz file, skip random(1..MAX_SKIP) lines, return next URL."""
-    start_line = random.randint(1, MAX_SKIP)
+    extract_at = sorted(random.sample(range(1, MAX_SKIP + 1), num_urls))
     gz_url = f"{BASE_URL}/{subset}/{gz_file}"
+    urls = []
+    domains = []  # To avoid duplicates
     with requests.get(gz_url, headers={"User-Agent": UA},
                       stream=True, timeout=60) as r:
         r.raise_for_status()
         with gzip.GzipFile(fileobj=r.raw) as gzf:
             for i, line in enumerate(gzf, start=1):
-                if i < start_line:
+                if i > MAX_SKIP:
+                    break
+                
+                if i not in extract_at:
                     continue
+
                 parts = line.decode("utf-8", "ignore").split(" ")
+
                 if len(parts) < 4:
+                    extract_at[extract_at.index(i)] += 1 # Try the next line
                     continue
+
                 ctx = parts[3].lstrip("<").rstrip("> .")
-                if ctx.startswith("http"):
-                    return ctx                    # first usable URL
-    return None                                   # rare: file exhausted
+
+                if not ctx.startswith("http"):
+                    extract_at[extract_at.index(i)] += 1 # Try the next line
+                    continue
+                
+                # Avoid duplicates by checking the domain
+                domain = urllib.parse.urlparse(ctx).netloc
+                if domain not in domains and domain not in DOMAINS_VISITED:
+                    domains.append(domain)
+                    urls.append(ctx)
+
+            return urls
 
 def fetch_html(url: str) -> str:
     try:
-        resp = requests.get(url, headers={"User-Agent": UA},timeout=30)
+        resp = requests.get(url, headers={"User-Agent": UA},timeout=5)
         resp.raise_for_status()
         return resp.text
     
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching {url}: {e}")
+        if PRINT_URLS:
+            print(f"Error fetching {url}: {e}")
         return None
 
 def is_english(html: str) -> bool:
@@ -86,44 +133,68 @@ def is_english(html: str) -> bool:
 
 # -------------------------------------------------------------------
 def main():
-    i = 0
-    while i < NUM_SAMPLES:
-        # 1. choose a random subset and a random gz_file
-        subset = random.choice(WDC_Subsets)
+    for (subset, num_samples) in zip(WDC_Subsets, NUM_SAMPLES):
+        if num_samples <= 0:
+            continue
+
+        # 1. choose a random gz_file
         gz_files = list_gz_files(subset)
         if not gz_files:
             raise RuntimeError(f"No gz_files found for {subset}")
         gz_file  = random.choice(gz_files)
         print()
-        print(f"Subset = {subset}  |  gz_file = {gz_file}")
+        print(f"Subset = {subset}  |  gz_file = {gz_file} |  num_samples = {num_samples}")
         
         # 2. pick a random URL
-        url = random_url_from_gz_file(subset, gz_file)
-        if not url:
+        urls = random_url_from_gz_file(subset, gz_file, num_samples*100) # *10 creates a buffer incase websites are not usable  # Shuffle to randomize the order
+        if len(urls) == 0:
             raise RuntimeError("Couldn’t find a usable URL – try again")
-        print("Chosen URL:", url)
+        random.shuffle(urls)
 
-        # 3. download raw HTML
-        html = fetch_html(url)
+        print("Downloading HTML")
+        num_samples_saved = 0
+        for url in urls:
+            url_variations = [url, "https://" + urllib.parse.urlparse(url).netloc]
+            for url_variation in url_variations:
+                if PRINT_URLS:
+                    print()
+                # 3. download raw HTML
+                if PRINT_URLS:
+                    print("Chosen URL:", url_variation)
 
-        if not html:
-            print("No HTML")
-            continue
+                html = fetch_html(url_variation)
 
-        if "itemscope" not in html:
-            print("No Mircodata")
-            continue
+                if not html:
+                    if PRINT_URLS:
+                        print("No HTML")
+                    continue
 
-        if not is_english(html):
-            print("Not English")
-            continue
+                if "itemscope" not in html:
+                    if PRINT_URLS:
+                        print("No Microdata")
+                    continue
 
-        # 4. save HTML to disk
-        out  = OUT_DIR / subset / f"{urllib.parse.quote_plus(url)}.html"
-        out.write_text(html, encoding="utf-8", errors="ignore")
-        print("Saved HTML →", out.resolve())
+                if not is_english(html):
+                    if PRINT_URLS:
+                        print("Not English")
+                    continue
 
-        i += 1
+                # 4. save HTML to disk
+                out  = OUT_DIR / subset / f"{urllib.parse.quote_plus(url_variation)}.html"
+                out.write_text(html, encoding="utf-8", errors="ignore")
+
+                with open("domains_visited.csv", "a") as f:
+                    writer = csv.writer(f)
+                    writer.writerow([urllib.parse.urlparse(url_variation).netloc])
+
+                if PRINT_URLS:
+                    print("Saved HTML →", out.resolve())
+
+                num_samples_saved += 1
+                print(num_samples_saved, end=", ")
+            
+            if num_samples_saved >= num_samples*len(url_variations):
+                break
 
 if __name__ == "__main__":
     main()
