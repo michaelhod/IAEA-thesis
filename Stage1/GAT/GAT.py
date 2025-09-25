961d725a77b2e58bc91ea52aba68e467d3f37803
Michael Hodgins <michaelhodgins@live.co.uk>
2025-09-25 21:27:56 +0100

    highthenlowdrooutpairloss being trained

--- Stage1/GAT/GAT.py
+++ Stage1/GAT/GAT.py
@@ -502,7 +502,7 @@ def train_model(model,
 
     print(model)
 
-    model_path = "./model_in_traininghighdroout.pt"
+    model_path = "./model_in_traininghighthenlowdrooutpairloss.pt"
     if os.path.exists(model_path) and load_checkpoint:
         print("loading existing model...")
         model.load_state_dict(torch.load(model_path))
@@ -510,24 +510,24 @@ def train_model(model,
     model.to(device)
     
     opt   = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
-    sched = lr_scheduler.OneCycleLR(opt, max_lr=4e-4, epochs=num_epochs, steps_per_epoch=len(train_loader),
-                   pct_start=0.1, anneal_strategy='cos', div_factor=25, final_div_factor=1e4, cycle_momentum=False)
+    sched = lr_scheduler.OneCycleLR(opt, max_lr=4e-5, epochs=num_epochs, steps_per_epoch=len(train_loader),
+                   pct_start=0.1, anneal_strategy='cos', div_factor=15, final_div_factor=1e3, cycle_momentum=False)
                         #StepLR(opt, step_size=3, gamma=0.9)
-    criterion = focal_loss
-    # criterion = PairwiseAUCFocalLoss(
-    #             gamma=2.0,
-    #             alpha=0.25,
-    #             lambda_focal=0.9,  # 0 ⇒ pure ranking loss; 1 ⇒ equal weight
-    #             sample_k=None     # speeds up training; set None for exact loss
-    #         )
+    #criterion = focal_loss
+    criterion = PairwiseAUCFocalLoss(
+                gamma=2.0,
+                alpha=0.25,
+                lambda_focal=0.9,  # 0 ⇒ pure ranking loss; 1 ⇒ equal weight
+                sample_k=5999     # speeds up training; set None for exact loss
+            )
     #criterion = nn.BCEWithLogitsLoss()
 
     best_f1, fig_ax, best_state = 0.0, None, None
     train_loss, val_loss, precision, recall, f1score = [], [], [], [], []
 
     for epoch in range(1, num_epochs + 1):
-        lambda_title = 0.02 if epoch > 0 else 0
-        p_Lef_drop = 0.9 - 0.7 * (epoch-0)/(num_epochs-0 + 1e-9)        
+        lambda_title = 0.02 if epoch > 2 else 0
+        p_Lef_drop = 0.3 - 0.3 * (epoch-0)/(num_epochs-0 + 1e-9)        
         use_E_attr,  use_A_attr = (epoch>0), (epoch>0)
 
         loss = train_epoch(model, train_loader, opt, criterion, sched, epoch, num_epochs, device=device, use_E_attr=use_E_attr, use_A_attr = use_A_attr, p_Lef_drop = p_Lef_drop, lambda_title=lambda_title)
@@ -557,7 +557,7 @@ def train_model(model,
                 train_loss,
                 val_loss,
                 precision,recall,f1score,
-                "TrueTransformer-newedgeshighdroout",
+                "TrueTransformer-highthenlowdrooutpairloss",
                 xlabel="Epoch",
                 ylabel_left="Loss",
                 ylabel_right="P · R · F1",
@@ -613,7 +613,7 @@ _, trainloss, valloss, fig_ax = train_model(model,
 
 # %%
 #Save model
-torch.save(model.state_dict(), "TrueTransformer-neweedgeshighdroout.pt")
+torch.save(model.state_dict(), "TrueTransformer-highthenlowdrooutpairloss.pt")
 
 # %%
 # model_path = "./FULLTRAINEDALLDATAModelf1-74-learning.pt"
